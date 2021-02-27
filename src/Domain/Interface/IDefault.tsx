402e4ac944b83abc44bc51202acd6c5a1da066fe
Gustavo Pilla <gustavopillacoutinhogmail.com>
2021-02-27 09:32:22 -0300

    Added to Domains Interface Default a validation of Id and Created Date.

--- src/Domain/Interface/IDefault.tsx
+++ src/Domain/Interface/IDefault.tsx
@@ -1,6 +1,9 @@
 import DefaultError from "../Validator/DefaultError";
 
 export default interface IDefault {
-    isValid() : boolean;
-    getErrors() : DefaultError[];
+    isValid(): boolean;
+    getErrors(): DefaultError[];
+
+    validateId(value?: string): void;
+    validateCreatedAt(value?: string): void;
 }
